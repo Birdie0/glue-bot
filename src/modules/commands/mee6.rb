0f0dddfbddd0d44e9594060795cf100fe745d0df
Birdie0 <Birdie0@users.noreply.github.com>
2018-01-30 12:13:02 +0200

    fix

--- src/modules/commands/mee6.rb
+++ src/modules/commands/mee6.rb
@@ -31,28 +31,6 @@ module Bot
         end
       end
 
-      def get_lvl_xp(xp)
-        5 * (xp**2) + 50 * xp + 100
-      end
-
-      def get_xp_info(player)
-        remaining = player['xp']
-        level = 0
-        while remaining >= get_lvl_xp(level)
-          remaining -= get_lvl_xp(level)
-          level += 1
-        end
-        OpenStruct.new(
-          name: "#{player['username']}##{player['discriminator']}",
-          level: level,
-          total_xp: player['xp'],
-          remaining: remaining,
-          level_xp_max: get_lvl_xp(level),
-          xp: get_lvl_xp(level) - remaining,
-          percent: (get_lvl_xp(level) - remaining) * 100 / get_lvl_xp(level)
-        )
-      end
-
       # mee6 leaderboard command
       command(:meelead) do |event, server_id|
         server_id ||= event.server.id
