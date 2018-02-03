036c7596f97841dda69158677ed2d789e5f544f3
Birdie0 <Birdie0@users.noreply.github.com>
2018-02-03 16:30:59 +0200

    webhook info command test

---
+++ src/modules/commands/w.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Bot
+  module DiscordCommands
+    # Basic info about bot.
+    module W
+      extend Discordrb::Commands::CommandContainer
+
+      base_url = 'https://birdie0.github.io/discord-webhooks-guide'
+      INFO = YAML.load_file('files/webhook.yml')
+
+      command(:w, help_available: false) do |event, element|
+        info = INFO.find { |i| i['name'] == element }
+        if info
+          event.channel.send_embed('') do |embed|
+            embed.title = info['name']
+            embed.description = info['description']
+            embed.add_field(
+              name: 'Example',
+              value: info['example']
+            )
+            embed.image = Discordrb::Webhooks::EmbedImage.new(
+              url: base_url + info['image']
+            )
+            embed.color = 0x00897b
+          end
+        end
+        nil
+      end
+
+    end
+  end
+end
