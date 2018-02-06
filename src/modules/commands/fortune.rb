af572417705823ebf01cf8c994d059cd556cd961
Birdie0 <Birdie0@users.noreply.github.com>
2018-02-06 14:01:52 +0200

    -_-

--- src/modules/commands/fortune.rb
+++ src/modules/commands/fortune.rb
@@ -2,13 +2,13 @@
 
 module Bot
   module DiscordCommands
-    # Basic info about bot.
+    # Fortune command
     module Fortune
       extend Discordrb::Commands::CommandContainer
 
       command(:fortune,
-              description: 'Info about the bot',
-              usage: "#{BOT.prefix}about") do |event, category|
+              description: 'gives you fortune cookie',
+              usage: "#{BOT.prefix}fortune [category]") do |event, category|
         cookie = fortune(category)
         event.channel.send_embed('') do |embed|
           embed.title = "Here's your cookie! <:fortunecookie:362519486278008832>"
