203f476ef68530f12cae9e4b2f9cceb07498c7c3
Birdie0 <Birdie0@users.noreply.github.com>
2018-07-01 22:57:23 +0300

    fix meelead

--- src/modules/mee6_functions.rb
+++ src/modules/mee6_functions.rb
@@ -13,9 +13,9 @@ def get_xp_info(player)
     name: "#{player['username'][0...15]}##{player['discriminator']}",
     level: level,
     total_xp: player['xp'],
-    remaining: remaining,
+    remaining: get_lvl_xp(level) - remaining,
     level_xp_max: get_lvl_xp(level),
-    xp: get_lvl_xp(level) - remaining,
+    xp: remaining,
     percent: (remaining * 100.0 / get_lvl_xp(level)).round(2)
   )
 end
